f8d294a28ea409816c5428fb0a05a44ada494f10
Tong_RulE76 <thanapol.phu@gmail.com>
2023-04-28 13:43:45 +0900

    fix bug and put comment

--- MDF_evaluation (ACR)/detect flare_matrix_profile.R
+++ MDF_evaluation (ACR)/detect flare_matrix_profile.R
@@ -1,3 +1,4 @@
+# Load required package
 library(tsmp)
 
 # Define constants
@@ -8,12 +9,13 @@ top_discords <- 1
 # Extract data from the list of dataframes
 temp_df <- data.frame(list_of_dfs[index_file][1])
 
-
+# Apply Time Series Matrix Profile (tsmp) algorithm
 mp <- tsmp(temp_df$z_instances, window_size = WINDOW_SIZE, verbose = 0)
 discords_data <- discords(mp)
 
-# get top discord
-top_discord_index = discords_data$discord$discord_idx[top_discords][[1]]
+# get the index of the top discord
+top_discord_index <- discords_data$discord$discord_idx[top_discords][[1]]
+
 # Plot the data
 plot(x = temp_df$time_from_peak, y = temp_df$z_instances, type = "l",
      xlab = "Time from flare peak (sec)",
@@ -25,6 +27,5 @@ abline(v = 0, col = "blue", lwd = 1, lty = 2)
 
 # Add a red line for the discord region
 lines(x = c(temp_df$time_from_peak[top_discord_index:(top_discord_index + WINDOW_SIZE)]),
-      y = temp_df$z_instances[temp_df$time_from_peak[top_discord_index:(top_discord_index + WINDOW_SIZE)]],
+      y = temp_df$z_instances[top_discord_index:(top_discord_index + WINDOW_SIZE)],
       col = "red")
-
